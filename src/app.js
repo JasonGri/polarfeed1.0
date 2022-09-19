55cc0d7a593bf827722f52ff02932673d8db8361
JasonGri <Iason.Grivas@acg.edu>
2022-09-19 17:13:41 +0300

    Configured html css and js for the frontend

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 module.exports = () => {
+  app.set("views", path.join(__dirname, "/views"));
+  app.set("view engine", "ejs");
+
+  app.use("/assets", express.static(path.join(__dirname, "public")));
+
   app.get("/", (req, res, next) => {
-    res.send("Up and running");
+    res.render("pages/homepage");
   });
 
   return app;
